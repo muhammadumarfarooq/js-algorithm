2f9cf2a3047bb7deadafdea1cf76b8cd99713d83
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-11-17 12:11:51 +0500

    show 5 odd numbers using for loop

--- script.js
+++ script.js
@@ -22,3 +22,7 @@ let hour = 7;
 if (hour >= 6 && hour < 12) console.log("Good Morning");
 else if (hour >= 12 && hour < 18) console.log("Good Afternoon");
 else console.log("Good Evening");
+
+// 3. Show 5 odd number using for loops
+
+for (let i = 1; i <= 10; i++) if (i % 2 !== 0) console.log(i);
