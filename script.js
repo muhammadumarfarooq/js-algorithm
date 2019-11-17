a7ae979b263c117a285a4d36d32b6e784db60e70
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-11-17 10:51:04 +0500

    show message based on condition

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ b = c;
 
 console.log(a);
 console.log(b);
+
+// 2. Printing Message based on time
+//PROBLEM
+//if hour is between 6am and 12pm: Good Morning!
+//if hour is between 12pm and 6pm: Good Afternoon
+//Otherwise: Good Evening
+
+// 24 hour clock
+
+let hour = 7;
+
+if (hour >= 6 && hour < 12) console.log("Good Morning");
+else if (hour >= 12 && hour < 18) console.log("Good Afternoon");
+else console.log("Good Evening");
