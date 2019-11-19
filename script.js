16da2e739e16751eaf584ef244c423a1f04181f5
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-11-19 15:35:43 +0500

    make a function that takes two numbers adn  return a number which is maximum

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1. swapping variables
+//===== 1. swapping variables =====
 console.log("========== Swapping Variables ==========");
 
 let a = "red";
@@ -11,7 +11,8 @@ b = c;
 console.log(a);
 console.log(b);
 
-// 2. Printing Message based on time
+//===== 2. Printing Message based on time =====
+
 //PROBLEM
 //if hour is between 6am and 12pm: Good Morning!
 //if hour is between 12pm and 6pm: Good Afternoon
@@ -26,11 +27,23 @@ if (hour >= 6 && hour < 12) console.log("Good Morning");
 else if (hour >= 12 && hour < 18) console.log("Good Afternoon");
 else console.log("Good Evening");
 
-// 3. Show 5 odd number using for loops
+//===== 3. Show 5 odd number using for loops =====
 console.log("========== Odd Numbers Using For Loop ==========");
 
 for (let i = 1; i <= 10; i++) if (i % 2 !== 0) console.log(i);
 
-// 4. Show 5 even numbers in reverse order using for loop
+//===== 4. Show 5 even numbers in reverse order using for loop =====
 console.log("========== Even Numbers in Reverse Order ==========");
 for (let i = 10; i >= 1; i--) if (i % 2 === 0) console.log(i);
+
+//===== 5. Write a function that takes two numbers and return maximum of the two
+
+const maxNum = calcMaxNum(4, 3);
+console.log(maxNum);
+
+function calcMaxNum(a, b) {
+  // if (a > b) return a;
+  // return b;
+
+  return a > b ? a : b;
+}
