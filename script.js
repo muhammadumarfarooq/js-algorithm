3518f9cc103156f0805447ff9276e35c477a9863
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-11-20 15:11:55 +0500

    demerits point

--- script.js
+++ script.js
@@ -79,3 +79,23 @@ function fizzBuzz(input) {
   else if (input % 5 === 0) return "Buzz";
   return input;
 }
+
+// ===== Demerits Point =====
+
+console.log("========== Demerits Point ==========");
+
+checkSpeed(90);
+
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+  if (speed < speedLimit + kmPerPoint) {
+    console.log("OK");
+    return;
+  }
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+
+  points >= 12
+    ? console.log("Licence Suspended")
+    : console.log("Point: " + points);
+}
