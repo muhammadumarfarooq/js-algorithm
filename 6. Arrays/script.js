62c8d37b21052908160f8503e1873ddee98f4763
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-12-15 11:07:00 +0500

    array from range

--- 6. Arrays/script.js
+++ 6. Arrays/script.js
@@ -1,131 +1,144 @@
-// Topics: adding, removing, finding, emptying, combining , slicing, iteration, Joining an array
+// // Topics: adding, removing, finding, emptying, combining , slicing, iteration, Joining, sorting, Testing an elements of array, filter, mapping over an array
 
-//Adding Elements
+// //Adding Elements
 
-const colors = ["red", "green", "blue"];
+// const colors = ["red", "green", "blue"];
 
-colors.push("orange"); //add item at the end
-colors.unshift("voilet"); //add item at the beginning
+// colors.push("orange"); //add item at the end
+// colors.unshift("voilet"); //add item at the beginning
 
-colors.splice(2, 1, "dil"); //add item at any location
+// colors.splice(2, 1, "dil"); //add item at any location
 
-// console.log(colors);
+// // console.log(colors);
 
-// Finding Elements (Primitives)
+// // Finding Elements (Primitives)
 
-const numbers = [1, 2, 3, 4, 5, 1, 1, 5, 6, 1];
-// console.log(numbers.indexOf(2));
-// console.log(numbers.lastIndexOf(1));
+// const numbers = [1, 2, 3, 4, 5, 1, 1, 5, 6, 1];
+// // console.log(numbers.indexOf(2));
+// // console.log(numbers.lastIndexOf(1));
 
-// console.log(numbers.indexOf(2) !== -1);
-// console.log(numbers.includes(2));
+// // console.log(numbers.indexOf(2) !== -1);
+// // console.log(numbers.includes(2));
 
-// Finding Elements (Reference Type)
+// // Finding Elements (Reference Type)
 
-const courses = [
-  { id: 1, name: "a" },
-  { id: 2, name: "b" }
-];
+// const courses = [
+//   { id: 1, name: "a" },
+//   { id: 2, name: "b" }
+// ];
 
-const found = courses.find(course => course.name === "a");
+// const found = courses.find(course => course.name === "a");
 
-// console.log(found);
+// // console.log(found);
 
-// Combining Arrays:
+// // Combining Arrays:
 
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5];
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5];
 
-const newArr = arr1.concat(arr2);
-// console.log(newArr);
+// const newArr = arr1.concat(arr2);
+// // console.log(newArr);
 
-// Slicing Array
+// // Slicing Array
 
-let slicedArr = newArr.slice(1, 3);
-slicedArr = newArr.slice(3); //start from index 3 and slice till end
-slicedArr = newArr.slice(); //copy whole array
+// let slicedArr = newArr.slice(1, 3);
+// slicedArr = newArr.slice(3); //start from index 3 and slice till end
+// slicedArr = newArr.slice(); //copy whole array
 
-// console.log(slicedArr);
+// // console.log(slicedArr);
 
-// Spread operator  (another easy and flexible way of copy and concating an array)
-const combine = [...arr1, ...arr2];
-const copy = [...combine, "dsfads"];
+// // Spread operator  (another easy and flexible way of copy and concating an array)
+// const combine = [...arr1, ...arr2];
+// const copy = [...combine, "dsfads"];
 
-// console.log(copy);
+// // console.log(copy);
 
-// Iterating over an array => use for-of loop
+// // Iterating over an array => use for-of loop
 
-const names = ["Umar", "Hassan", "Umair"];
+// const names = ["Umar", "Hassan", "Umair"];
 
-// for (let name of names) {
-//   console.log(item);
-// }
+// // for (let name of names) {
+// //   console.log(item);
+// // }
 
-// names.forEach((name, index) => console.log(index, name));
+// // names.forEach((name, index) => console.log(index, name));
 
-// Joining an Array
+// // Joining an Array
 
-const name = "Muhammad Umar Farooq";
-const splitArr = name.split(" ");
-const joinStr = splitArr.join("-");
+// const name = "Muhammad Umar Farooq";
+// const splitArr = name.split(" ");
+// const joinStr = splitArr.join("-");
 
-// const info = ["Umar", "malik", 21, 91];
+// // const info = ["Umar", "malik", 21, 91];
 
-// const joined = info.join(" - ");
+// // const joined = info.join(" - ");
 
-// console.log(info);
-// console.log(joined);
+// // console.log(info);
+// // console.log(joined);
 
-// console.log(joinStr);
+// // console.log(joinStr);
 
-// Sorting Array
-const myNum = [2, 3, 1];
+// // Sorting Array
+// const myNum = [2, 3, 1];
 
-myNum.sort();
-myNum.reverse();
+// myNum.sort();
+// myNum.reverse();
 
-// console.log(myNum);
+// // console.log(myNum);
 
-const persons = [
-  { id: 1, name: "zinger" },
-  { id: 2, name: "pista" },
-  { id: 2, name: "aista" }
-];
+// const persons = [
+//   { id: 1, name: "zinger" },
+//   { id: 2, name: "pista" },
+//   { id: 2, name: "aista" }
+// ];
 
-persons.sort((a, b) => {
-  if (a.name < b.name) return -1;
-  if (a.name > b.name) return 1;
-  return 0;
-});
+// persons.sort((a, b) => {
+//   if (a.name < b.name) return -1;
+//   if (a.name > b.name) return 1;
+//   return 0;
+// });
 
-// console.log(persons);
+// // console.log(persons);
 
-// Testing the elements of an array
+// // Testing the elements of an array
 
-// .every ==> all have to fullfill condition
-// .some ==> find atleast one
+// // .every ==> all have to fullfill condition
+// // .some ==> find atleast one
 
-const numbersA = [-1, 1, -2, -3, -4, -5];
+// const numbersA = [-1, 1, -2, -3, -4, -5];
 
-const atLeastOnePositive = numbersA.every(function(value) {
-  return value >= 0;
-});
+// const atLeastOnePositive = numbersA.every(function(value) {
+//   return value >= 0;
+// });
 
-// console.log(atLeastOnePositive);
+// // console.log(atLeastOnePositive);
 
-// Mapping over an array
+// // Mapping over an array
 
-const numbersB = [-1, 1, -2, -3, -4, -5];
+// const numbersB = [-1, 1, -2, -3, -4, -5];
 
-const items = numbersB
-  .filter(value => value <= 0)
-  .map(value => ({
-    number: value
-  }));
+// const items = numbersB
+//   .filter(value => value <= 0)
+//   .map(value => ({
+//     number: value
+//   }));
 
-// const items = filtered.map(value => "<li>" + value + "</li>");
-// const html = "<ul>" + items.join("") + "</ul>";
+// // const items = filtered.map(value => "<li>" + value + "</li>");
+// // const html = "<ul>" + items.join("") + "</ul>";
 
-// console.log(filtered);
-console.log(items);
-// console.log(html);
+// // console.log(filtered);
+// // console.log(items);
+// // console.log(html);
+
+// Exercises
+//========== Array From Range ==========
+
+const numbers = arrayFromRange(-10, -40);
+
+console.log(numbers);
+
+function arrayFromRange(min, max) {
+  const numberArray = [];
+  for (let i = min; i <= max; i++) numberArray.push(i);
+  return numberArray;
+}
